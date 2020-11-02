078d80d34b655d9d685daff5db9eb5bc92267cc0
Emanuel <e.rista@outlook.it>
2020-11-02 17:01:30 +0100

    Completed

---
+++ script.js
@@ -0,0 +1,6 @@
+$('.fa-bars').click(function(){//se clicchi sulla fa-bars (hamburger) allora si attiva la funzione:
+  $('.hamburger-menu').show();//mostra il il menu hamburger
+})
+$('.close').click(function(){//se flicchi sulla fa-times(X, chiudi) allora si attiva la funzione:
+  $('.hamburger-menu').hide();//nascondi il menu hamburger
+})
